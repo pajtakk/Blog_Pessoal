f6c4d0726449b670babf071726b1fc20495fb710
pajtak <fehpaj@gmail.com>
2022-08-01 12:09:49 -0300

    blog pessoal

---
+++ src/main/java/com/blogpessoal/BlogpessoallApplication.java
@@ -0,0 +1,13 @@
+package com.blogpessoal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogpessoallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogpessoallApplication.class, args);
+	}
+
+}
